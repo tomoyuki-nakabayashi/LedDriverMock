d5b3bc19b7103ed6fa8cfa1a7fd3f7b279ba685f
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2017-09-04 21:03:27 +0900

    first commit

---
+++ src/LedDriver.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+void LedDriverCreate(uint16_t *addr);
+void LedDriverTurnOn(uint16_t ledNumber);
+void LedDriverTurnAllOn(void);
+void LedDriverTurnAllOff(void);
+void LedDriverTurnOff(uint16_t ledNumber);
+bool LedDriverIsOn(uint16_t ledNumber);
+bool LedDriverIsOff(uint16_t ledNumber);
